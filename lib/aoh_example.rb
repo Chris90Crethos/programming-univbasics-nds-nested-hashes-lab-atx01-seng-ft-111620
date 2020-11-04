667e73760998b73ebe96089d597e9cab2598258b
Chris Martin <bmg.martin08@gmail.com>
2020-11-04 02:55:42 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,6 +25,7 @@ def literal_aoh
     { name:  "Joelle van Dyne", occupation: "Radio Personality"},
     { name:  "Pat Monteseian", occupation: "Staff" },
     { name:  "Kate Gompert", occupation: "None" },
+    
     ]
 end
 
